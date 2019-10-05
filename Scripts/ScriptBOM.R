b8c516afdd8c1c35fb205edf42a19a23d22216be
Kaur <kau019@csiro.au>
2019-10-05 21:07:52 +1000

    Finished question 3 of challenge

--- Scripts/ScriptBOM.R
+++ Scripts/ScriptBOM.R
@@ -4,12 +4,13 @@ BOM_data <- read_csv("data/BOM_data.csv")# assign a variable
 # Question1- solution1
 BOM_data_separated <- separate(BOM_data, col = Temp_min_max, into = c("Tempmin", "Tempmax"), sep = "/") #separate temperatures min and max
 BOM_data_separated #print the dataset
-BOM_data_separated %>% #filter out the non-numeric values
+BOM_data_filtered <- BOM_data_separated %>% #filter out the non-numeric values
   filter(Tempmin != '-') %>%
   filter(Tempmax != '-') %>%
   filter(Rainfall != '-') %>%
   group_by(Station_number) %>% #group by station numbers
   summarise (n = n()) #count the number of rows
+BOM_data_filtered
 
 #Question 2- solution 1
 BOM_data_separated %>% # Start with BOM data seperated 
@@ -23,7 +24,7 @@ BOM_data_separated %>% # Start with BOM data seperated
 
 #Question 3
 BOM_stations %>%
-gather (station_ID, value, -info)
+gather (station_number, value, -info)
 BOM_stations
 
 BOM_stations <- read_csv("data/BOM_stations.csv") #read data set1
@@ -33,8 +34,13 @@ gather (station_number, values, -info)
 stations_tidy <- stations_very_long %>%
   spread(info, values)
 stations_tidy
-stations_tidy_numeric <- mutate (stations_tidy, station_number = as.numeric (station_number))
+stations_tidy_numeric <- mutate (stations_tidy, station_number = as.numeric(station_number))
 stations_tidy_numeric
-BOM_combined_2 <- left_join(BOM_data_separated, stations_tidy)
-
-BOM_combined_2
+BOM_stations_all <- full_join(BOM_data_separated,stations_tidy_numeric, by = c("Station_number" = "station_number")) #but the station number in the two data files is different (chr vs dbl) 
+q3_ans <- BOM_stations_all %>% 
+mutate(t_diff = as.numeric(tempmax) - as.numeric(tempmin)) %>%
+filter(!is.na(t_diff)) %>% 
+group_by(state) %>% 
+summarise(avg_t_diff = mean(t_diff)) %>% 
+arrange(avg_t_diff)
+q3_ans
